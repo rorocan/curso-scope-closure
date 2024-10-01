855ab41a947f5f55b05a27225cf080a17d35ecd4
Rodrigo Rojas <rorocanrojas1998@gmail.com>
2024-10-01 12:00:40 -0500

    main

--- src/scope/block.js
+++ src/scope/block.js
@@ -3,10 +3,11 @@ function fruits() {
       var fruit1 = 'Apple'; // function scope
       let fruit2 = 'Kiwi'; // block scope
       const fruit3 = 'Banana'; // block scope
+    }  
+      console.log(fruit1);
       console.log(fruit2);
       console.log(fruit3);
-    }
-    console.log(fruit1);
+   
   }
   
   fruits();
