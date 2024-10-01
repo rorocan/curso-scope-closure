a63037dd7a13e10c1bc0f4bacea70a89c01c7d65
Rodrigo Rojas <rorocanrojas1998@gmail.com>
2024-10-01 11:43:22 -0500

    main

---
+++ src/scope/function.js
@@ -0,0 +1,10 @@
+function greeting() {
+    let userName = 'Thalia';
+    console.log(userName);
+
+    if (userName === 'Thalia'){
+        console.log(`Hello ${userName}!`);
+    }
+}
+
+greeting();
